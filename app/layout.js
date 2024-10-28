4eee4398e08ed60c3e7294ab3acf4f1d2b9287f1
ohwojero <ohwojerogodstime@gmail.com>
2024-10-28 14:40:41 +0100

    portfolio

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { FloatingNavDemo } from "./components/FloatingNavDemo";
 import { ThemeProvider } from "./Provider";
-import Footer from "./components/Footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
